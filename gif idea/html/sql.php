d066afc34823ff104c49f9b17ec0344507b068af
Edward Digby <edward.jdigby@tiscali.co.uk>
2014-02-16 19:47:20 +0000

    Commented out debugging code

--- gif idea/html/sql.php
+++ gif idea/html/sql.php
@@ -2,12 +2,13 @@
 // Create connection
 $con=mysqli_connect("remote.ejdigby.com","yrs","9gkYB9p@*d0","imagetag");
 
-// Check connection
+/* Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 else {
   echo "Connected!!";
-}
+} */
 ?>
+
